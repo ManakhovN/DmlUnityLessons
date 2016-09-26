1f5ba0439382bfb2c59baa358b0a4d0bed349ef7
Надир <nadirmanakhov@gmail.com>
2016-09-26 07:49:12 +0300

    trying to add personage controller.

---
+++ Assets/Lesson3/Scripts/PersonageController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonageController : MonoBehaviour {
+    public float movingSpeed = 1f;
+    Rigidbody2D rigidBody;
+    public void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    Vector2 movingVector = Vector2.zero;
+    void FixedUpdate()
+    {
+        movingVector.x = Time.fixedDeltaTime * movingSpeed * Input.GetAxis("Horizontal");
+        rigidBody.AddForce(movingVector, ForceMode2D.Force);
+    }
+}
