ae0b6b9a425481d20c7cb1ffd3db379464bd8477
Надир <nadirmanakhov@gmail.com>
2016-09-26 08:19:30 +0300

    added camera moving script.

---
+++ Assets/Lesson3/Scripts/CameraMoving.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMoving : MonoBehaviour {
+    Vector3 movingPosition;
+    public Transform target;
+    public float speed;
+	void Start () {
+        movingPosition.z = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movingPosition.x = target.position.x;
+        movingPosition.y = target.position.y; 
+        transform.position = Vector3.Lerp(transform.position, movingPosition, Time.deltaTime * speed);
+	}
+}
