bcbf41d4285efaf246d1527614c2435d865365e2
Надир <nadirmanakhov@gmail.com>
2016-09-26 08:35:21 +0300

    added jumping.

--- Assets/Lesson3/Scripts/PersonageController.cs
+++ Assets/Lesson3/Scripts/PersonageController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PersonageController : MonoBehaviour {
     public float movingSpeed = 1f;
     Rigidbody2D rigidBody;
+    public bool isFlying = true;
     public void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
@@ -12,6 +13,20 @@ public class PersonageController : MonoBehaviour {
     void FixedUpdate()
     {
         movingVector.x = Time.fixedDeltaTime * movingSpeed * Input.GetAxis("Horizontal");
+        if (!isFlying && Input.GetKeyDown(KeyCode.Space))
+            movingVector.y = 400f;
+        else movingVector.y = 0f;
         rigidBody.AddForce(movingVector, ForceMode2D.Force);
+        isFlying = true;
+    }
+
+    void OnCollisionStay2D(Collision2D coll) {
+        if (coll.contacts.Length > 1)
+        {
+            Vector2 collisionPosition = transform.InverseTransformPoint(coll.contacts[0].point);
+            Vector2 collisionPosition2 = transform.InverseTransformPoint(coll.contacts[1].point);
+            if (collisionPosition.y == collisionPosition2.y)
+                isFlying = false;
+        }
     }
 }
