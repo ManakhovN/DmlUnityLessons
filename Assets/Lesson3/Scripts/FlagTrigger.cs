cee65130061d92737a3530d238aa25a175b7ae11
Надир Манахов <nadirmanakhov@gmail.com>
2016-09-27 11:44:45 +0300

    about triggers;

---
+++ Assets/Lesson3/Scripts/FlagTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlagTrigger : MonoBehaviour {
+    SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    public void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        spriteRenderer.sprite = sprites[1];
+    }
+    public void OnTriggerExit2D(Collider2D collider)
+    {
+        spriteRenderer.sprite = sprites[0];
+    }
+}
