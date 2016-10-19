d755e25e6d304435f97a54340411983437929a0f
Ruslan <ahmetsharipov.ruslan@gmail.com>
2016-10-19 12:24:44 +0300

    no message

---
+++ Assets/Lesson5/ActivateSound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateSound : MonoBehaviour {
+    public AudioSource leftFoot;
+    public AudioSource rightFoot;
+	
+    public void LeftFootStep()
+    {
+        leftFoot.Play();
+    }
+    public void RightFootStep()
+    {
+        rightFoot.Play();
+    }
+}
